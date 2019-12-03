66d30bccf0f45383f844a957dd9488352a03aa4d
cody-a-ray <cody.a.ray@outlook.com>
2019-12-03 12:20:53 -0800

    Navbar handlebar

---
+++ public/templates/404pageTemplate.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['404page'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<main class=\"error-container\">\r\n  <div class = \"404_image\"></div><br />\r\n    <div>\r\n      <img src=\"https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif\" alt=\"404_gif\" />\r\n    </div>\r\n</main>\r\n";
+},"useData":true});
+})();
