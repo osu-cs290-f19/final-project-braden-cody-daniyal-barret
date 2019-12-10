8946c382541c1d1a3b0c48b4a7d5b50afde2fc4c
cody-a-ray <cody.a.ray@outlook.com>
2019-12-09 18:47:40 -0800

    More favorites stuff and snackbar

--- public/main.js
+++ public/main.js
@@ -1,4 +1,191 @@
+var allBooks = [];
+var favorites = [];
+
+// On adding new book via modal, this function grabs the data values from the modal, inserts it into the DOM, and sends it to the back end for storage
+function handleModalAccept() {
+    var newBook = getNewBookValsFromModal();
+    insertNewBook(newBook.title, newBook.author, newBook.subject, newBook.photoURL, newBook.photoURL, newBook.vendorURL);
+    storeBook(newBook.title, newBook.author, newBook.photoURL, newBook.subject);
+    hideAddBookModal();
+}
+
+// Gets book data values from modal
+function getNewBookValsFromModal() {
+    var bookVals = {
+        author: document.getElementById('input-author').value.trim(),
+        title: document.getElementById('input-title').value.trim(),
+        subject: document.getElementById('input-subject').value.trim(),
+        photoURL: document.getElementById('input-photoURL').value.trim(),
+        favorite: false
+    };
+    if (!bookVals.author || !bookVals.title || !bookVals.subject || !bookVals.photoURL) {
+        alert('One or more fields are blank!');
+        return undefined;
+    }
+    bookVals.vendorURL = createAmazonURL(bookVals.title);
+    return bookVals;
+};
+
+// Creates a URL to an Amazon search for the input title
+function createAmazonURL(title) {
+    var titleWords = title.split(' ');
+    var vendorURL = "https://www.amazon.com/s?k=";
+    for (var i = 0; i < titleWords.length; i++) {
+        vendorURL += titleWords[i].toLowerCase();
+        if (i !== titleWords.length) {
+            vendorURL += '+';
+        }
+    }
+    vendorURL += "&ref=nb_sb_noss_2";
+    return vendorURL;
+}
+
+// Creates a new bookCard with Handlebars and inserts it into DOM
+function insertNewBook(title, author, subject, photoURL, vendorURL, favorite) {
+    bookCardHTML = Handlebars.templates.bookCard({
+        title: title,
+        author: author,
+        subject: subject,
+        photoURL: photoURL,
+        vendorURL: vendorURL,
+        favorite: favorite
+    });
+
+    var bookCardContainer = document.querySelector('#books');
+    bookCardContainer.insertAdjacentHTML('beforeend', bookCardHTML);
+}
+
+// Sends information for a new book to server to be stored in JSON file
+function storeBook(title, author, photoURL, subject) {
+    var req = new XMLHttpRequest();
+    req.open('POST', '/');
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function(event) {
+        if (event.target.status !== 200) {
+            var message = event.target.response;
+            alert("Error storing book data: " + message);
+        }
+    });
+    req.send(JSON.stringify({
+        title: title,
+        author: author,
+        photoURL: photoURL,
+        subject: subject
+    }));
+};
+
+function showAddBookModal() {
+    var addBookModal = document.getElementById('add-a-book-modal');
+    addBookModal.classList.remove('hidden');
+}
+
+function hideAddBookModal() {
+    var addBookModal = document.getElementById('add-a-book-modal');
+    addBookModal.classList.add('hidden');
+    clearModalFields();
+}
+
+function clearModalFields() {
+    document.getElementById('input-author').value = ""
+    document.getElementById('input-title').value = ""
+    document.getElementById('input-subject').value = ""
+    document.getElementById('input-photoURL').value = ""
+}
+
+/* When a heart is clicked in a bookCard, this function sends a request to the server to change the 'favorite' status in JSON file
+   of the book for which it was clicked. It then either adds or removes the bookCard from the favorites page and shows a snackbar message */
+function handleFavoriteClick(event) {
+    var bookId = event.target.parentNode.parentNode.parentNode.getAttribute('data-id');
+    var req = new XMLHttpRequest();
+    req.open('POST', '/favorites/' + bookId);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function(event) {
+        if (event.target.status !== 200) {
+            var message = event.target.response;
+            alert("Error processing request: " + message);
+        }
+    });
+    req.send(JSON.stringify({
+        id: bookId
+    }));
+
+    favorites = [];
+    var favorited; // A boolean value to keep track of whether the book was 'favorited' or 'unfavorited'
+    var targetBook; // The title of the book that was 'favorited' or 'unfavorited'
+    allBooks.forEach(function(book) {
+        if (book.id === Number(bookId)) {
+            book.favorite = !book.favorite;
+            favorited = book.favorite;
+            targetBook = book.title;
+        }
+        if (book.favorite === true) {
+            favorites.push(book);
+        }
+    });
+
+    var snackbarMessage = targetBook;
+    if (favorited) {
+        snackbarMessage += " was added to your favorites!";
+    } else {
+        snackbarMessage += " was removed from your favorites!";
+    }
+
+    if (document.getElementById('content-container').getAttribute('page') == 'favorites') {
+        renderFavorites();
+    }
+    showSnackbar(snackbarMessage);
+}
+
+// Removes and reinserts all favorites into DOM
+function renderFavorites() {
+    var bookContainer = document.getElementById('books');
+    while (bookContainer.lastChild) {
+        bookContainer.removeChild(bookContainer.lastChild);
+    }
+    favorites.forEach(function(book) {
+        insertNewBook(book.title, book.author, book.subject, book.photoURL, book.vendorURL, book.favorite);
+    })
+}
+
+// Shows snackbar with message about which book what 'favorited' or 'unfavorited'
+function showSnackbar(message) {
+    var snackbar = document.getElementById("snackbar");
+    snackbar.innerText = message;
+    snackbar.className = "show";
+    setTimeout(function() {
+        snackbar.className = snackbar.className.replace("show", "");
+    }, 1500);
+}
+
+// Builds a book object from data attributes of a bookCard element in the DOM
+function parseBookElm(bookElm) {
+    var book = {
+        id: Number(bookElm.getAttribute('data-id')),
+        title: bookElm.getAttribute('data-title'),
+        author: bookElm.getAttribute('data-author'),
+        subject: bookElm.getAttribute('data-subject'),
+        vendorURL: bookElm.getAttribute('data-vendorURL'),
+        favorite: Boolean(bookElm.getAttribute('data-favorite'))
+    };
+
+    var bookImgElm = bookElm.querySelector('.card-image img');
+    book.photoURL = bookImgElm.src;
+
+    return book;
+}
+
 window.addEventListener('DOMContentLoaded', function() {
+
+    // Initializes the allBooks array and the favorites array by filling them with books in the DOM
+    var bookElms = document.getElementsByClassName('card');
+    for (var i = 0; i < bookElms.length; i++) {
+        var book = parseBookElm(bookElms[i]);
+        allBooks.push(book);
+        if (book.favorite === true) {
+            favorites.push(bookElms[i]);
+        }
+    }
+
     var addBookButton = document.getElementById('add-a-book-button');
     if (addBookButton) {
         addBookButton.addEventListener('click', showAddBookModal);
@@ -17,113 +204,7 @@ window.addEventListener('DOMContentLoaded', function() {
     var favButton = document.querySelectorAll('.favorite-button');
     if (favButton) {
         favButton.forEach(function(currentValue) {
-            currentValue.addEventListener('click', handleFavoriteButton);
+            currentValue.addEventListener('click', handleFavoriteClick);
         });
     };
 });
-
-function showSnackbar() {
-    var snackbar = document.getElementById("snackbar");
-
-    snackbar.className = "show";
-
-    setTimeout(function() { console.log('FIRED!!!'); }, 3000);
-}
-
-function handleFavoriteButton(event) {
-    var bookId = event.target.parentNode.parentNode.parentNode.getAttribute('data-id');
-    var req = new XMLHttpRequest();
-    req.open('POST', '/favorites/' + bookId);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load', function(event) {
-        if (event.target.status !== 200) {
-            var message = event.target.response;
-            alert("Error processing request: " + message);
-        }
-    });
-    req.send(JSON.stringify({
-        id: bookId
-    }));
-    //window.location.reload(false);
-    showSnackbar();
-}
-
-function handleModalAccept() {
-    var newBook = getNewBookVals();
-
-    insertNewBook(newBook.title, newBook.photoURL);
-    storeBook(newBook.title, newBook.photoURL);
-    hideAddBookModal();
-}
-
-function showAddBookModal() {
-    var addBookModal = document.getElementById('add-a-book-modal');
-    addBookModal.classList.remove('hidden');
-}
-
-function getNewBookVals() {
-    var bookVals = {
-        author: document.getElementById('input-author').value.trim(),
-        title: document.getElementById('input-title').value.trim(),
-        subject: document.getElementById('input-subject').value.trim(),
-        photoURL: document.getElementById('input-photoURL').value.trim(),
-        favorite: false
-    };
-
-    if (!bookVals.author || !bookVals.title || !bookVals.subject || !bookVals.photoURL) {
-        alert('One or more fields are blank!');
-        return undefined;
-    }
-
-    var vendorURL = "https://www.amazon.com/s?k=";
-    var titleWords = bookVals.title.split(' ');
-    for (var i = 0; i < titleWords.length; i++) {
-        vendorURL += titleWords[i].toLowerCase();
-        if (i !== titleWords.length) {
-            vendorURL += '+';
-        }
-    }
-    bookVals.vendorURL += "&ref=nb_sb_noss_2";
-
-    return bookVals;
-
-};
-
-function insertNewBook(title, photoURL) {
-    bookCardHTML = Handlebars.templates.bookCard({
-        title: title,
-        photoURL: photoURL
-    });
-
-    var bookCardContainer = document.querySelector('#books');
-    bookCardContainer.insertAdjacentHTML('beforeend', bookCardHTML);
-}
-
-function storeBook(title, photoURL) {
-    var req = new XMLHttpRequest();
-    req.open('POST', '/');
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load', function(event) {
-        if (event.target.status !== 200) {
-            var message = event.target.response;
-            alert("Error storing book data: " + message);
-        }
-    });
-    req.send(JSON.stringify({
-        title: title,
-        photoURL: photoURL
-    }));
-};
-
-function hideAddBookModal() {
-    var addBookModal = document.getElementById('add-a-book-modal');
-    addBookModal.classList.add('hidden');
-    clearModalFields();
-}
-
-function clearModalFields() {
-    document.getElementById('input-author').value = ""
-    document.getElementById('input-title').value = ""
-    document.getElementById('input-subject').value = ""
-    document.getElementById('input-photoURL').value = ""
-}
--- public/templates/bookCardTemplate.js
+++ public/templates/bookCardTemplate.js
@@ -5,12 +5,16 @@ templates['bookCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(cont
 
   return "<div class=\"col s12 m4 l4 books\">\r\n    <div class=\"card\" data-id="
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data,"loc":{"start":{"line":2,"column":30},"end":{"line":2,"column":36}}}) : helper)))
-    + " data-title="
-    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":2,"column":48},"end":{"line":2,"column":57}}}) : helper)))
-    + " data-author="
-    + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data,"loc":{"start":{"line":2,"column":70},"end":{"line":2,"column":80}}}) : helper)))
-    + " data-subject="
-    + alias4(((helper = (helper = helpers.subject || (depth0 != null ? depth0.subject : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"subject","hash":{},"data":data,"loc":{"start":{"line":2,"column":94},"end":{"line":2,"column":105}}}) : helper)))
+    + " data-title=\""
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":2,"column":49},"end":{"line":2,"column":58}}}) : helper)))
+    + "\" data-author=\""
+    + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data,"loc":{"start":{"line":2,"column":73},"end":{"line":2,"column":83}}}) : helper)))
+    + "\" data-subject=\""
+    + alias4(((helper = (helper = helpers.subject || (depth0 != null ? depth0.subject : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"subject","hash":{},"data":data,"loc":{"start":{"line":2,"column":99},"end":{"line":2,"column":110}}}) : helper)))
+    + "\" data-vendorURL=\""
+    + alias4(((helper = (helper = helpers.vendorURL || (depth0 != null ? depth0.vendorURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"vendorURL","hash":{},"data":data,"loc":{"start":{"line":2,"column":128},"end":{"line":2,"column":141}}}) : helper)))
+    + "\" data-favorite="
+    + alias4(((helper = (helper = helpers.favorite || (depth0 != null ? depth0.favorite : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"favorite","hash":{},"data":data,"loc":{"start":{"line":2,"column":157},"end":{"line":2,"column":169}}}) : helper)))
     + ">\r\n        <div class=\"card-image\">\r\n            <img src=\""
     + alias4(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data,"loc":{"start":{"line":4,"column":22},"end":{"line":4,"column":34}}}) : helper)))
     + "\">\r\n            <span class='card-title'>\r\n                <span id='title'>"
